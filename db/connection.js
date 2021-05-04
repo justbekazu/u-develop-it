0dde8e6c04fcfd2f98d6e7cd6ac0be67f2467e57
djkaz <justbekazu@gmail.com>
2021-05-03 20:48:30 -0500

    seperation of interest

---
+++ db/connection.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'alexander0718',
+      database: 'election',
+
+
+   },
+    console.log('Connected to the election database.')
+  );
+
+  module.exports = db;
--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const mysql = require('mysql2');
+
 const inputCheck = require('./utils/inputCheck');
 
+const db = require('./db/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -11,19 +12,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
         //Connect to database
-    const db = mysql.createConnection(
-     {
-       host: 'localhost',
-       // Your MySQL username,
-       user: 'root',
-       // Your MySQL password
-       password: 'alexander0718',
-       database: 'election',
 
-
-    },
-     console.log('Connected to the election database.')
-   );
 
 
 
